9a68d3bd56b3b83e039332788b5a1c04e0a2909b
Stacey Lin <twstacey@gmail.com>
2015-08-26 12:48:18 -0700

    Recipe page, FizzBuzz ruby code

---
+++ Recipe/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  // Check off ingredient list
+  $('.ingredients li').click(function(){
+    var ingredient = $(this);
+    var obtained = ingredient.hasClass('obtained');
+    if (!obtained){
+      ingredient.addClass('obtained');
+      ingredient.find("input").prop("checked", true);
+    } else {
+      ingredient.removeClass('obtained');
+      ingredient.find("input").prop("checked", false);
+    }
+  });
+
+  // Show current step in directions
+  $('.directions li').click(function(){
+    var currentDirection = $(this);
+    $('.directions li.current').removeClass('current');
+    currentDirection.addClass('current');
+  });
+
+  // Hide all photos on click 
+  var photosHidden = false;
+  $('.txt-only').click(function(){
+    if (!photosHidden){
+      $(this).text('Show Photos');
+      $('.img').hide();
+      photosHidden = true;
+    } else {
+      $(this).text('Hide Photos');
+      $('.img').show();
+      photosHidden = false;
+    }
+  });
+});
---
+++ work/fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,7 @@
+(1..100).each do |number|
+  if number % 3 == 0
+    puts number % 5 == 0 ? ("FizzBuzz") : ("Fizz")
+  else
+    puts number % 5 == 0 ? ("Buzz") : (n)
+  end
+end
